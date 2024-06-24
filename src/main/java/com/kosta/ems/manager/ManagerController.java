363d28decb7d34653a7872688874acf6109918b0
hojin <hojin7java@gmail.com>
2024-06-24 15:44:08 +0900

    manager Mapper, Service 완료

---
+++ src/main/java/com/kosta/ems/manager/ManagerController.java
@@ -0,0 +1,7 @@
+package com.kosta.ems.manager;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ManagerController {
+}
---
+++ src/main/java/com/kosta/ems/manager/ManagerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.kosta.ems.manager;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ManagerServiceImpl implements ManagerService {
+	private final ManagerMapper managerMapper;
+
+	@Override
+	public Map<String, String> login(String employeeNumber, String password) {
+		return managerMapper.login(employeeNumber, password);
+	}
+
+}
