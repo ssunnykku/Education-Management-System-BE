b96fc80806f39b1c9e4733503e9369f1c97b23cd
HelloNuri.dvl <HelloNuri.dvl@gmail.com>
2024-06-24 16:43:42 +0900

    Manage Controller 완료

--- src/main/java/com/kosta/ems/manager/ManagerController.java
+++ src/main/java/com/kosta/ems/manager/ManagerController.java
@@ -1,7 +1,48 @@
 package com.kosta.ems.manager;
 
-import org.springframework.stereotype.Controller;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 
-@Controller
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/manager")
+@RequiredArgsConstructor
 public class ManagerController {
+	private final ManagerService service;
+	
+	//Controller처럼 작동함
+	@PostMapping("/login")
+	public void login(@RequestBody Map<String, String> loginRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		Map<String, String> map = service.login(loginRequest.get("employeeNumber"), loginRequest.get("password"));
+		if(Objects.isNull(map)) {
+			response.sendRedirect("/ui/login");
+			return;
+		}
+		HttpSession session = request.getSession();
+		session.setAttribute("managerId", map.get("managerId"));
+		session.setAttribute("academyLocation", map.get("academyLocation"));
+		System.out.println("managerId: " + map.get("managerId"));
+		System.out.println("managerId: " + map.get("academyLocation"));
+		response.sendRedirect("/ui/notifications");
+	}
+	
+	@PostMapping("/logout")
+	public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		HttpSession session = request.getSession();
+		if(session != null) {
+			session.invalidate();
+		}
+		response.sendRedirect("/ui/login");
+	}
 }
