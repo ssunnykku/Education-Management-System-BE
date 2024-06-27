8ebb537d0bf33951bfa7ff55dcc7ce018d985e6c
HelloNuri.dvl <HelloNuri.dvl@gmail.com>
2024-06-27 14:16:18 +0900

    feat: controller 카테고리 리스트 완료

--- src/main/java/com/kosta/ems/studentPoint/StudentPointController.java
+++ src/main/java/com/kosta/ems/studentPoint/StudentPointController.java
@@ -1,14 +1,19 @@
 package com.kosta.ems.studentPoint;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.kosta.ems.student.StudentService;
+import com.kosta.ems.studentPoint.dto.PointCategoryDTO;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,9 +22,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/managers")
+@RequestMapping("/points")
 @RequiredArgsConstructor
 @Slf4j
 public class StudentPointController {
 	private final StudentPointService service;
+	private final StudentService studnetService;
+	
+	@GetMapping("/category-list")
+	public Map<String, List<PointCategoryDTO>> pointCategoryList(){
+		return Map.of("data", service.getPointCategoryList());
+	}
+	
 }
