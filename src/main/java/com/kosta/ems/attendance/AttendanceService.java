bfeb0fbc03e2c1bec1fb5c312bccbbfed25d9029
yoojinyang0303 <yoojinyang0303@gmail.com>
2024-06-27 17:48:19 +0900

    feat: 특정일 수강생 출결상태 조회 service 작성, 테스트 완료

--- src/main/java/com/kosta/ems/attendance/AttendanceService.java
+++ src/main/java/com/kosta/ems/attendance/AttendanceService.java
@@ -20,6 +20,25 @@ public interface AttendanceService {
     // [출결] - 수강생 출석 조회 목록 조회
     Collection<StudentAttendanceListDTO> getStudentAttendanceList(String name, int courseNumber, int page, int size);
     
+    // [출결] - 특정일의 수강생 출석 상태 목록 조회 (for 출결 입력/수정)
+    // 경우1 _ 기수+수강생명 입력
+    // 검색 결과 개수 가져오기 (for 페이지네이션)
+    int selectCourseNumberAndStudentNameListAmount(String attendanceDate, String academyLocation, String name, int courseNumber);
+    // 검색 결과 데이터 목록 가져오기
+    Collection<AttendanceListBySearchFilterDTO> selectCourseNumberAndStudentNameList(String attendanceDate, String academyLocation, String name, int courseNumber, int page, int size);
+    
+    // 경우2 _ 기수 또는 수강생명 입력
+    // 검색 결과 개수 가져오기 (for 페이지네이션)
+    int selectCourseNumberOrStudentNameListAmount(String attendanceDate, String academyLocation, String name, int courseNumber);
+    // 검색 결과 데이터 목록 가져오기
+    Collection<AttendanceListBySearchFilterDTO> selectCourseNumberOrStudentNameList(String attendanceDate, String academyLocation, String name, int courseNumber, int page, int size);
+    
+    // 경우3 _ 기수+수강생명 미입력
+    // 검색 결과 개수 가져오기 (for 페이지네이션)
+    int selectDateAndLocationListAmount(String attendanceDate, String academyLocation, String name, int courseNumber);
+    // 검색 결과 데이터 목록 가져오기
+    Collection<AttendanceListBySearchFilterDTO> selectDateAndLocationList(String attendanceDate, String academyLocation, String name, int courseNumber, int page, int size);
+    
     // [출결] - 선택한 수강생의 출석 상태 수정
-    void updateStudentAttendance(String attendanceStatus, LocalDate attendanceDate, String studentId);
+    void updateStudentAttendance(String attendanceStatus, String attendanceDate, String studentId);
 }
--- src/main/java/com/kosta/ems/attendance/AttendanceServiceImpl.java
+++ src/main/java/com/kosta/ems/attendance/AttendanceServiceImpl.java
@@ -39,16 +39,78 @@ public class AttendanceServiceImpl implements AttendanceService {
     	return attendanceMapper.selectStudentAttendanceList(name, courseNumber, page, size);
     }
     
+    // [출결] - 특정일의 수강생 출석 상태 목록 조회 (for 출결 입력/수정)
+    // 경우1 _ 기수+수강생명 입력
+    // 검색 결과 개수 가져오기 (for 페이지네이션)
+    @Override
+    public int selectCourseNumberAndStudentNameListAmount(String attendanceDate, String academyLocation, String name, int courseNumber) {
+    	int year = Integer.parseInt(attendanceDate.split("-")[0] );
+    	int month = Integer.parseInt(attendanceDate.split("-")[1]);
+    	int day = Integer.parseInt(attendanceDate.split("-")[2]);
+    	
+    	return attendanceMapper.selectCourseNumberAndStudentNameListAmount(LocalDate.of(year, month, day), academyLocation, name, courseNumber);
+    }
+    // 검색 결과 데이터 목록 가져오기
+    @Override
+    public Collection<AttendanceListBySearchFilterDTO> selectCourseNumberAndStudentNameList(String attendanceDate, String academyLocation, String name, int courseNumber, int page, int size) {
+    	int year = Integer.parseInt(attendanceDate.split("-")[0] );
+    	int month = Integer.parseInt(attendanceDate.split("-")[1]);
+    	int day = Integer.parseInt(attendanceDate.split("-")[2]);
+    	
+    	return attendanceMapper.selectCourseNumberAndStudentNameList(LocalDate.of(year, month, day), academyLocation, name, courseNumber, page, size);
+    }
+    
+    // 경우2 _ 기수 또는 수강생명 입력
+    // 검색 결과 개수 가져오기 (for 페이지네이션)
+    @Override
+    public int selectCourseNumberOrStudentNameListAmount(String attendanceDate, String academyLocation, String name, int courseNumber) {
+    	int year = Integer.parseInt(attendanceDate.split("-")[0] );
+    	int month = Integer.parseInt(attendanceDate.split("-")[1]);
+    	int day = Integer.parseInt(attendanceDate.split("-")[2]);
+    	
+    	return attendanceMapper.selectCourseNumberOrStudentNameListAmount(LocalDate.of(year, month, day), academyLocation, name, courseNumber);
+    }
+    // 검색 결과 데이터 목록 가져오기
+    @Override
+    public Collection<AttendanceListBySearchFilterDTO> selectCourseNumberOrStudentNameList(String attendanceDate, String academyLocation, String name, int courseNumber, int page, int size) {
+    	int year = Integer.parseInt(attendanceDate.split("-")[0] );
+    	int month = Integer.parseInt(attendanceDate.split("-")[1]);
+    	int day = Integer.parseInt(attendanceDate.split("-")[2]);
+    	
+    	return attendanceMapper.selectCourseNumberOrStudentNameList(LocalDate.of(year, month, day), academyLocation, name, courseNumber, page, size);
+    }
+    
+    // 경우3 _ 기수+수강생명 미입력
+    // 검색 결과 개수 가져오기 (for 페이지네이션)
+    @Override 
+    public int selectDateAndLocationListAmount(String attendanceDate, String academyLocation, String name, int courseNumber) {
+    	int year = Integer.parseInt(attendanceDate.split("-")[0] );
+    	int month = Integer.parseInt(attendanceDate.split("-")[1]);
+    	int day = Integer.parseInt(attendanceDate.split("-")[2]);
+    	
+    	return attendanceMapper.selectDateAndLocationListAmount(LocalDate.of(year, month, day), academyLocation, name, courseNumber);
+    }
+    // 검색 결과 데이터 목록 가져오기
+    @Override
+    public Collection<AttendanceListBySearchFilterDTO> selectDateAndLocationList(String attendanceDate, String academyLocation, String name, int courseNumber, int page, int size) {
+    	int year = Integer.parseInt(attendanceDate.split("-")[0] );
+    	int month = Integer.parseInt(attendanceDate.split("-")[1]);
+    	int day = Integer.parseInt(attendanceDate.split("-")[2]);
+    	
+    	return attendanceMapper.selectDateAndLocationList(LocalDate.of(year, month, day), academyLocation, name, courseNumber, page, size);
+    }
+    
+    
     // [출결] - 선택한 수강생의 출석 상태 수정
     @Override
-    public void updateStudentAttendance(String attendanceStatus, LocalDate attendanceDate, String studentId) {
-    	/*
+    public void updateStudentAttendance(String attendanceStatus, String attendanceDate, String studentId) {
+    	
     	int year = Integer.parseInt(attendanceDate.split("-")[0]);
         int month = Integer.parseInt(attendanceDate.split("-")[1]);
         int day = Integer.parseInt(attendanceDate.split("-")[2]);
-        */
-    	// UpdateStudentAttendanceStatusDTO dto = UpdateStudentAttendanceStatusDTO.builder().attendanceStatus(attendanceStatus).attendanceDate(LocalDate.of(year, month, day)).studentId(studentId).build();
-    	UpdateStudentAttendanceStatusDTO dto = UpdateStudentAttendanceStatusDTO.builder().attendanceStatus(attendanceStatus).attendanceDate(attendanceDate).studentId(studentId).build();
+        
+    	UpdateStudentAttendanceStatusDTO dto = UpdateStudentAttendanceStatusDTO.builder().attendanceStatus(attendanceStatus).attendanceDate(LocalDate.of(year, month, day)).studentId(studentId).build();
+    	// UpdateStudentAttendanceStatusDTO dto = UpdateStudentAttendanceStatusDTO.builder().attendanceStatus(attendanceStatus).attendanceDate(attendanceDate).studentId(studentId).build();
     	attendanceMapper.updateStudentAttendance(dto);
     }
 }
