142a5588671cdcc1f7c2d5f0ffdd76ccd70cc35b
yoojinyang0303 <yoojinyang0303@gmail.com>
2024-06-28 02:06:46 +0900

    feat: 출결_출결 상태 검색 목록 controller 구현 및 해당 부분 js 일부 작성

--- src/main/java/com/kosta/ems/attendance/AttendanceController.java
+++ src/main/java/com/kosta/ems/attendance/AttendanceController.java
@@ -6,11 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.kosta.ems.student.NoSuchDataException;
 import com.kosta.ems.student.PageRequestDTO;
@@ -32,11 +28,9 @@ public class AttendanceController {
 		Map<String, Object> result = new HashMap<String, Object>();
 		int page = pageRequest.getPage()-1;  // page는 실제로 0부터 시작하기 때문
 		int size = pageRequest.getSize();
-		
-		
+
 		// 페이징 response
 		int totalCount = attendanceServiceImpl.getStudentAttendanceListAmount(name, courseNumber);
-		// int totalCount = attendanceServiceImpl.getStudentAttendanceList(name, courseNumber);
 		int totalPage = (totalCount/size) + 1;
 		// int currentPage = pageRequest.getCurrentPage();
 		int currentPage = 2;  // 브라우저에서 받아올 값인데 아직 연결안해서 controller 테스트를 위해 작성했던 코드.
@@ -59,7 +53,67 @@ public class AttendanceController {
 		
 		return result;
 	}
-	
+
+	// [출결] - 출결 검색(조건: 날짜, 기수, 수강생명) 데이터 목록 가져오기 -- POSTMAN 테스트 완료
+	@PostMapping("attendances/search-list")
+	public Map<String, Object> getFilteredAttendanceList(@RequestBody Map<String, Object> request) {
+		Map<String, Object> result = new HashMap<String, Object>();
+
+		// request JSON 분리 (pageRequest, attendanceRequest)
+		Map<String, Object> pageRequest = (Map<String, Object>) request.get("pageRequest");
+		Map<String, Object> attendanceRequest = (Map<String, Object>) request.get("attendanceRequest");
+
+		int page = ((int) pageRequest.get("page"))-1;  // page는 실제로 0부터 시작하기 때문
+		int size = (int) pageRequest.get("size");
+
+		// 검색 경우(1~3) 파악
+		int courseNumber = (int) attendanceRequest.get("courseNumber");
+		String name = (String) attendanceRequest.get("name") == "" ? "none" : (String) attendanceRequest.get("name");
+		int totalCount = 0;
+
+		if(courseNumber != -1 && name == "none") {
+			// 경우1: 기수, 수강생명 모두 입력한 검색
+			System.out.println(">> 경우1");
+			totalCount = attendanceServiceImpl.selectCourseNumberAndStudentNameListAmount((String) attendanceRequest.get("attendanceDate"), (String) attendanceRequest.get("academyLocation"), (String) attendanceRequest.get("name"), (int)attendanceRequest.get("courseNumber"));
+
+			// 수강생 출결 목록 데이터
+			result.put("data", attendanceServiceImpl.selectCourseNumberAndStudentNameList((String) attendanceRequest.get("attendanceDate"), (String) attendanceRequest.get("academyLocation"), (String) attendanceRequest.get("name"), (int) attendanceRequest.get("courseNumber"), page, size));
+		} else if(courseNumber == -1 && name == "none") {
+			// 경우3: 기수, 수강생명 모두 미입력 (날짜만) 검색
+			System.out.println(">> 경우3");
+			totalCount = attendanceServiceImpl.selectDateAndLocationListAmount((String) attendanceRequest.get("attendanceDate"), (String) attendanceRequest.get("academyLocation"), (String) attendanceRequest.get("name"), (int)attendanceRequest.get("courseNumber"));
+
+			// 수강생 출결 목록 데이터
+			result.put("data", attendanceServiceImpl.selectDateAndLocationList((String) attendanceRequest.get("attendanceDate"), (String) attendanceRequest.get("academyLocation"), (String) attendanceRequest.get("name"), (int) attendanceRequest.get("courseNumber"), page, size));
+		} else {
+			// 경우2: 기수 또는 수강생명 입력한 검색
+			System.out.println(">> 경우2");
+			totalCount = attendanceServiceImpl.selectCourseNumberOrStudentNameListAmount((String) attendanceRequest.get("attendanceDate"), (String) attendanceRequest.get("academyLocation"), (String) attendanceRequest.get("name"), (int)attendanceRequest.get("courseNumber"));
+
+			// 수강생 출결 목록 데이터
+			result.put("data", attendanceServiceImpl.selectCourseNumberOrStudentNameList((String) attendanceRequest.get("attendanceDate"), (String) attendanceRequest.get("academyLocation"), (String) attendanceRequest.get("name"), (int) attendanceRequest.get("courseNumber"), page, size));
+		}
+
+		// 페이징 response
+		int totalPage = (totalCount/size) + 1;
+		int currentPage = (int)pageRequest.get("currentPage");
+		int prevPage = 0;
+		int nextPage = 0;
+		if(currentPage > 1 && currentPage < totalPage) {
+			prevPage = currentPage - 1;
+			nextPage = currentPage + 1;
+		} else if(currentPage == totalPage) {
+			prevPage = currentPage - 1;
+		} else if(currentPage == 1) {
+			nextPage = currentPage + 1;
+		}
+
+		PageResponseDTO pageInfo = PageResponseDTO.builder().totalCount(totalCount).totalPage(totalPage).currentPage(currentPage).prevPage(prevPage).nextPage(nextPage).build();
+		result.put("pageInfo", pageInfo);
+
+		return result;
+	}
+
 	// [출결] - 선택한 수강생의 출석 상태 수정
 	@PutMapping("attendances/student-status")
 	public UpdateDeleteResultDTO updateStudentAttendance(@RequestBody RequestStudentAttendanceDTO request) {
@@ -69,7 +123,8 @@ public class AttendanceController {
 			int month = Integer.parseInt(request.getAttendanceDate().split(".")[1]);
 			int day = Integer.parseInt(request.getAttendanceDate().split(".")[2]);
 			
-			attendanceServiceImpl.updateStudentAttendance(request.getAttendanceStatus(), LocalDate.of(year, month, month), request.getStudentId());
+			// attendanceServiceImpl.updateStudentAttendance(request.getAttendanceStatus(), LocalDate.of(year, month, month), request.getStudentId());
+			attendanceServiceImpl.updateStudentAttendance(request.getAttendanceStatus(), request.getAttendanceDate(), request.getStudentId());
 		} catch (NoSuchDataException e) {
 			dto.setCode(ResCode.FAIL.value());
 			dto.setMessage("Fail: updateStudentAttendance");
