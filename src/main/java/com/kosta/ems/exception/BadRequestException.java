032dc55436c4170d04f9ac03d3a286d30de99a8f
ssunnykku <ssunnykku@gmail.com>
2024-07-09 11:05:49 +0900

    chore: 주석 제거

--- src/main/java/com/kosta/ems/exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.kosta.ems.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class BadRequestException extends Exception {
-
-}
--- src/main/java/com/kosta/ems/studentPoint/StudentPointController.java
+++ src/main/java/com/kosta/ems/studentPoint/StudentPointController.java
@@ -28,45 +28,44 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class StudentPointController {
-	private final StudentPointService studentPointService;
-	@GetMapping("/category-list")
-	public Map<String, List<PointCategoryDTO>> pointCategoryList(){
-		return Map.of("data", studentPointService.getPointCategoryList());
-	}
-	
-	@GetMapping("student-list")
-	public Map studentListWithPoints(
-	        @RequestParam(value = "page",           defaultValue = "1")    int     page,
-            @RequestParam(value = "pageSize",       defaultValue = "10")   int     pageSize,
-            @RequestParam(value = "courseNumber",   defaultValue = "0")    int     courseNumber,
-                                                                           String  studentName,
+    private final StudentPointService studentPointService;
+
+    @GetMapping("/category-list")
+    public Map<String, List<PointCategoryDTO>> pointCategoryList() {
+        return Map.of("data", studentPointService.getPointCategoryList());
+    }
+
+    @GetMapping("student-list")
+    public Map studentListWithPoints(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+            @RequestParam(value = "courseNumber", defaultValue = "0") int courseNumber,
+            String studentName,
             @RequestParam(value = "excludeExpired", defaultValue = "true") boolean excludeExpired,
-            HttpServletRequest request) 
-	{
-	    return Map.of("result", studentPointService.getStudentListWithPoint(courseNumber, studentName, page, pageSize, studentName));
-	}
-	
-	@GetMapping("/student")
-	public Map<String, List<PointHistoryDTO>> getPointHistoryByStudent(int studentCourseSeq, HttpServletRequest request){
-		List<PointHistoryDTO> histories = studentPointService.getPointHistory(studentCourseSeq, getAcademyOfLoginUser(request));
-		return Map.of("data", histories);
-	}
-	//일단 포인트를 하나씩 등록하도록 하고 차후 등록할 포인트들을 한번에 받기로 바꾸자.
-	@PostMapping("/student")
-	public Map<String, Boolean> addPointToStudent(@RequestBody Map<String, Integer> dto, HttpServletRequest request){
-		boolean result = studentPointService.insertStudentPoint(dto.get("pointSeq"), getManagerIdOfLoginUser(request), dto.get("studentCourseSeq"), getAcademyOfLoginUser(request));
-		return Map.of("data", result);
-	}
-	
-	private String getAcademyOfLoginUser(HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		return "가산";
-//		return (String) session.getAttribute("academyLocation");
-	}
-	
-	private String getManagerIdOfLoginUser(HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		return "d893bf71-2f8f-11ef-b0b2-0206f94be675";
-//		return (String) session.getAttribute("managerId");
-	}
+            HttpServletRequest request) {
+        return Map.of("result", studentPointService.getStudentListWithPoint(courseNumber, studentName, page, pageSize, studentName));
+    }
+
+    @GetMapping("/student")
+    public Map<String, List<PointHistoryDTO>> getPointHistoryByStudent(int studentCourseSeq, HttpServletRequest request) {
+        List<PointHistoryDTO> histories = studentPointService.getPointHistory(studentCourseSeq, getAcademyOfLoginUser(request));
+        return Map.of("data", histories);
+    }
+
+    //일단 포인트를 하나씩 등록하도록 하고 차후 등록할 포인트들을 한번에 받기로 바꾸자.
+    @PostMapping("/student")
+    public Map<String, Boolean> addPointToStudent(@RequestBody Map<String, Integer> dto, HttpServletRequest request) {
+        boolean result = studentPointService.insertStudentPoint(dto.get("pointSeq"), getManagerIdOfLoginUser(request), dto.get("studentCourseSeq"), getAcademyOfLoginUser(request));
+        return Map.of("data", result);
+    }
+
+    private String getAcademyOfLoginUser(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        return "가산";
+    }
+
+    private String getManagerIdOfLoginUser(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        return "d893bf71-2f8f-11ef-b0b2-0206f94be675";
+    }
 }
