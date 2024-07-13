2bd9b4e1565c6c5f125a7ca48f3ed696123cb54c
yoojinyang0303 <yoojinyang0303@gmail.com>
2024-07-13 20:49:36 +0900

    fix: 여러 수강생의 출결 상태 입력, 수정시 횟수만큼 alert 뜨는 문제 해결

--- src/main/java/com/kosta/ems/attendance/AttendanceService.java
+++ src/main/java/com/kosta/ems/attendance/AttendanceService.java
@@ -57,7 +57,8 @@ public interface AttendanceService {
     List<AttendanceListBySearchFilterDTO> getDateAndLocationList(String attendanceDate, String academyLocation, String name, int courseNumber, int page, int size);
 
     // [출결] - 선택한 수강생의 출석 상태 수정
-    void updateStudentAttendance(String attendanceStatus, String attendanceDate, int studentCourseSeq);
+    // void updateStudentAttendance(String attendanceStatus, String attendanceDate, int studentCourseSeq);
+    void updateStudentAttendance(List<RequestStudentAttendanceDTO> dto);
 
     // [출결 입력]
     // 1. 특정일의 출결 상태가 등록되지 않은 수강생 목록 가져오기
