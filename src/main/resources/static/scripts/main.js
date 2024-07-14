a21dfcb7c454c19ffd64257630e7a64bbc1b5816
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2024-07-14 20:24:02 +0900

    feat: main: 과정 정보 FE 연결

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -6,6 +6,8 @@ let month = today.getMonth() + 1;
 let date = today.getDate();
 let day;
 
+const currentDate = year + '-' + (month = month >= 10 ? month : '0' + month) + '-' + (date = date >= 10 ? date : '0' + date);
+
 switch (today.getDay()) {
     case 0:
         day = '일요일';
@@ -46,7 +48,6 @@ fetch("/managers", requestOptions)
     .then((res) => res.json())
     .then((data) => {
             const user = data.result;
-            console.log(user);
             let result = `<div id="manager-info-name-wrapper" class="manager-info-item">
                         <span>이름</span>
                         <span>${user.name}</span>
@@ -57,9 +58,36 @@ fetch("/managers", requestOptions)
                     </div>
                     <div id="manager-info-location-wrapper" class="manager-info-item">
                         <span class="manager-info-item">교육장 </span>
-                        <span id="manager-info-location">${user.academyLocation} 교육장</span>
+                        <span id="manager-info-location">${user.academyLocation}</span>
                     </div>`
             $("#manager-info-wrapper").append(result);
+
+            $('#course-info-location-wrapper').append(`<span id="course-info-location">${user.academyLocation} 교육장</span>`)
         }
     )
+    .catch((error) => console.error(error));
+
+
+/* 현재 과정 정보  */
+
+fetch("/courses/current-list?currentDate=" + currentDate, {
+    method: "GET",
+
+})
+    .then((res) => res.json())
+    .then((data) => {
+        const resultList = data.result;
+        let result;
+        for (const resultElement of resultList) {
+            console.log(resultElement);
+            result += `<tr>
+                <td>${resultElement.courseNumber}</td>
+                <td>${resultElement.courseName}</td>
+                <td>${resultElement.professorName}</td>
+                <td>${resultElement.courseStartDate}</td>
+                <td>${resultElement.courseEndDate}</td>
+            </tr>`
+        }
+        $("#course-info-table").append(result);
+    })
     .catch((error) => console.error(error));
