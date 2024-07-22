813aa3f20d6e328e10b3dbd6c0153affe8fc491e
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2024-07-23 01:44:18 +0900

    feat: jwtController studentId 적용

--- src/main/java/com/kosta/ems/api/JwtController.java
+++ src/main/java/com/kosta/ems/api/JwtController.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
+import com.kosta.ems.config.jwt.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -28,30 +30,29 @@ public class JwtController {
     private final ApiService service;
     @Value("${security.level}")
     private String SECURITY_LEVEL;
+    private final JwtTokenProvider jwtTokenProvider;
 
     //과정조회, 포인트조회
     @GetMapping("/course-list")
     @PreAuthorize("hasRole('STUDENT')")
-
-    public Map getAllTakenCourses() {
-        StudentInfoDTO loginUser = getLoginUser();
-        return Map.of("result", service.getAllTakenCoursesByStudentId(loginUser.getStudentId()));
+    public Map getAllTakenCourses(HttpServletRequest request) {
+        String loginUser = getLoginUser(request);
+        return Map.of("result", service.getAllTakenCoursesByStudentId(loginUser));
     }
 
     //출결조회
     @GetMapping("/attendance-list")
     @PreAuthorize("hasRole('STUDENT')")
-
-    public Map getAttendanceByMonth(LocalDate date) {
-        StudentInfoDTO loginUser = getLoginUser();
+    public Map getAttendanceByMonth(LocalDate date, HttpServletRequest request) {
+        String loginUser = getLoginUser(request);
 
         double monthAttendanceRate;
 
-        CourseDTO currentCourse = service.getCurrentCourse(loginUser.getStudentId());
+        CourseDTO currentCourse = service.getCurrentCourse(loginUser);
         if (currentCourse == null) {
             return Map.of("result", "[]");
         } else {
-            List<AttendanceHistoryResponse> attendances = service.getAttendanceByMonth(date, loginUser.getStudentId());
+            List<AttendanceHistoryResponse> attendances = service.getAttendanceByMonth(date, loginUser);
             int fullDaysOfMonth = 0;
             int attendanceDays = 0;
             LocalDate temp = LocalDate.of(date.getYear(), date.getMonth(), 1);
@@ -72,72 +73,66 @@ public class JwtController {
 
     @GetMapping("/total-attendance-rate")
     @PreAuthorize("hasRole('STUDENT')")
-
-    public Map getTotalAttendanceRate() {
-        StudentInfoDTO loginUser = getLoginUser();
-        return Map.of("result", service.getTotalAttendanceRate(loginUser.getStudentId()));
+    public Map getTotalAttendanceRate(HttpServletRequest request) {
+        String loginUser = getLoginUser(request);
+        return Map.of("result", service.getTotalAttendanceRate(loginUser));
     }
 
     //포인트조회
     @GetMapping("/point-history")
     @PreAuthorize("hasRole('STUDENT')")
-
-    public Map getPointHistory(int courseSeq) {
-        StudentInfoDTO loginUser = getLoginUser();
-        return Map.of("result", service.getPointHistory(courseSeq, loginUser.getStudentId()));
+    public Map getPointHistory(int courseSeq,HttpServletRequest request) {
+        String loginUser = getLoginUser(request);
+        return Map.of("result", service.getPointHistory(courseSeq, loginUser));
     }
     
     //마이페이지 정보 조회
     @GetMapping("/student")
     @PreAuthorize("hasRole('STUDENT')")
-
-    public Map getStudentInfo() {
-        StudentInfoDTO loginUser = getLoginUser();
+    public Map getStudentInfo(HttpServletRequest request) {
+        String loginUser = getLoginUser(request);
         return Map.of("result", loginUser);
     }
 
     //마이페이지 정보 수정 모달
     @PutMapping("/student")
     @PreAuthorize("hasRole('STUDENT')")
-    public Map updateStudentInfo(@RequestBody UpdateStudentInfoRequest dto) {
-        StudentInfoDTO loginUser = getLoginUser();
-        return Map.of("result", service.updateStudentContactInfo(loginUser.getStudentId(), dto));
+    public Map updateStudentInfo(@RequestBody UpdateStudentInfoRequest dto,HttpServletRequest request) {
+        String loginUser = getLoginUser(request);
+        return Map.of("result", service.updateStudentContactInfo(loginUser, dto));
     }
     
     //현재 수강중인 과정
     @GetMapping("/current-course")
     @PreAuthorize("hasRole('STUDENT')")
-    public Map GetCurrentCourse() {
-        StudentInfoDTO loginUser = getLoginUser();
-        return Map.of("result", service.getCurrentCourse(loginUser.getStudentId()));
+    public Map GetCurrentCourse(HttpServletRequest request) {
+        String loginUser = getLoginUser(request);
+        return Map.of("result", service.getCurrentCourse(loginUser));
     }
     //마이페이지 입실/
     @GetMapping("/time")
     @PreAuthorize("hasRole('STUDENT')")
-    public Map GetAttendanceTimeStatus() {
-        StudentInfoDTO loginUser = getLoginUser();
-        return Map.of("result", service.getAttendanceTimeStatus(loginUser.getStudentId()));
+    public Map GetAttendanceTimeStatus(HttpServletRequest request) {
+        String loginUser = getLoginUser(request);
+        return Map.of("result", service.getAttendanceTimeStatus(loginUser));
     }
 
     @PostMapping("/in-time")
     @PreAuthorize("hasRole('STUDENT')")
-    public Map recordInTime() {
-        StudentInfoDTO loginUser = getLoginUser();
-        return Map.of("result", service.addInTime(loginUser.getStudentId()));
+    public Map recordInTime(HttpServletRequest request) {
+        String loginUser = getLoginUser(request);
+        return Map.of("result", service.addInTime(loginUser));
     }
 
     @PostMapping("/out-time")
     @PreAuthorize("hasRole('STUDENT')")
-    public Map recordOutTime() {
-        StudentInfoDTO loginUser = getLoginUser();
-        return Map.of("result", service.addOutTime(loginUser.getStudentId()));
+    public Map recordOutTime(HttpServletRequest request) {
+        String loginUser = getLoginUser(request);
+        return Map.of("result", service.addOutTime(loginUser));
     }
 
-    private StudentInfoDTO getLoginUser() {
-        StudentInfoDTO loginUser;
-         loginUser = service.getStudentByStudentCourseSeq(19);
-
-        return loginUser;
+    private String getLoginUser(HttpServletRequest request) {
+        return jwtTokenProvider.getStudentId(request);
     }
 
 }
